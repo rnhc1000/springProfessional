39c31c6a5cb60ffbe0a1e579bad9680f5b1ac4be
Ricardo Ferreira <rnhc1000@hotmail>
2024-12-27 17:40:32 -0300

    [fix] 🐛: fix Order Entity

--- challengeone/src/main/java/br/dev/ferreiras/challengeone/entities/Order.java
+++ challengeone/src/main/java/br/dev/ferreiras/challengeone/entities/Order.java
@@ -9,7 +9,8 @@ public class Order {
   private Double basic;
   private Double discount;
 
-  public Order() {};
+  public Order() {
+  }
 
   public Order(Integer code, Double basic, Double discount) {
     this.code = code;
