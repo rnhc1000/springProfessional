a680ff356f09acdd903b4afa0d8b32ac73cbeef4
Ricardo Ferreira <rnhc1000@hotmail>
2025-01-05 13:46:18 -0300

    [chore] 🔧: paginated and by id search of products

--- dscommerce/src/main/java/br/dev/ferreiras/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/br/dev/ferreiras/dscommerce/controllers/ProductController.java
@@ -4,6 +4,8 @@ import br.dev.ferreiras.dscommerce.dto.ProductDTO;
 import br.dev.ferreiras.dscommerce.entities.Product;
 import br.dev.ferreiras.dscommerce.repositories.ProductRepository;
 import br.dev.ferreiras.dscommerce.services.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -25,6 +26,13 @@ public class ProductController {
 
   @GetMapping(value="/{id}")
   public ProductDTO findById(@PathVariable Long id) {
+
    return productService.findById(id);
+  }
+
+  @GetMapping
+  public Page<ProductDTO> findAll(Pageable pageable) {
+
+    return productService.findAll(pageable);
   }
 }
