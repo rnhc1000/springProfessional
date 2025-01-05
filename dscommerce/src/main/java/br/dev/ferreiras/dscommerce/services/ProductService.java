68f1019e4da1b74145e0e09f0529db0d37180d54
Ricardo Ferreira <rnhc1000@hotmail>
2025-01-05 18:03:57 -0300

    [chore] 🔧: add update method - getReferenceById

--- dscommerce/src/main/java/br/dev/ferreiras/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/br/dev/ferreiras/dscommerce/services/ProductService.java
@@ -4,12 +4,10 @@ import br.dev.ferreiras.dscommerce.dto.ProductDTO;
 import br.dev.ferreiras.dscommerce.entities.Product;
 import br.dev.ferreiras.dscommerce.repositories.ProductRepository;
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -36,5 +34,40 @@ public class ProductService {
     Page<Product> products = productRepository.findAll(pageable);
 
     return products.map(ProductDTO::new);
+    /*
+    25-01-05 Sun 13:39:21.861 WARN  SpringDataJacksonConfiguration$PageModule$WarningLoggingModifier Serializing PageImpl
+    instances as-is is not supported, meaning that there is no guarantee about the stability of the resulting JSON structure!
+	For a stable JSON structure, please use Spring Data's PagedModel (globally via @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO))
+	or Spring HATEOAS and Spring Data's PagedResourcesAssembler as documented in https://docs.spring.io/spring-data/commons/reference/repositories/core-extensions.html#core.web.pageables.
+
+     */
+  }
+
+  @Transactional
+  public ProductDTO insert(ProductDTO productDTO) {
+
+    Product product = new Product();
+    copyDtoToEntity(productDTO, product);
+    product = productRepository.save(product);
+
+    return new ProductDTO(product);
+  }
+
+  @Transactional
+  public ProductDTO update(Long id, ProductDTO productDTO) {
+
+    Product product = productRepository.getReferenceById(id);
+    copyDtoToEntity(productDTO, product);
+    product = productRepository.save(product);
+
+    return new ProductDTO(product);
+  }
+
+  private void copyDtoToEntity(ProductDTO productDTO, Product entity) {
+
+    entity.setName(productDTO.getName());
+    entity.setDescription(productDTO.getDescription());
+    entity.setPrice(productDTO.getPrice());
+    entity.setImgUrl(productDTO.getImgUrl());
   }
 }
