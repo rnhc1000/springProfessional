546b1556a841cdea15bba2a199aa572261ef03e1
Ricardo Ferreira <rnhc1000@hotmail>
2025-01-09 15:56:39 -0300

    [chore] 🔧: add UserDetailsService interface to UserService to implement loadUserByUsername throwing UsernameNotFoundException

--- dscommerce/src/main/java/br/dev/ferreiras/dscommerce/services/UserService.java
+++ dscommerce/src/main/java/br/dev/ferreiras/dscommerce/services/UserService.java
@@ -5,12 +5,14 @@ import br.dev.ferreiras.dscommerce.entities.User;
 import br.dev.ferreiras.dscommerce.projections.UserDetailsProjection;
 import br.dev.ferreiras.dscommerce.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
   private final UserRepository userRepository;
 
@@ -35,4 +37,21 @@ public class UserService {
     return user;
   }
 
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+    List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+
+    User user = new User();
+
+    user.setEmail(username);
+    user.setPassword(result.getFirst().getPassword());
+    for (UserDetailsProjection projection : result) {
+      user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+    }
+
+    return user;
+  }
+
 }
+
